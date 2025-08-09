7b404a81700b3551006753b2604bb290e87c80c3
Md. Minhazul Haque <mdminhazulhaque@gmail.com>
2025-08-09 22:00:34 +0600

    Refactor AWS regions: move from config.py to window.py and update .gitignore

--- window.py
+++ window.py
@@ -12,7 +12,26 @@ from PySide6.QtGui import QKeySequence, QShortcut, QIcon
 from ui_form import Ui_Window
 import resources_rc  # Import the compiled resource file
 
-from config import AWS_REGIONS
+# AWS Regions
+AWS_REGIONS = [
+    "us-east-1 (N. Virginia)",
+    "us-east-2 (Ohio)",
+    "us-west-1 (N. California)",
+    "us-west-2 (Oregon)",
+    "ap-south-1 (Mumbai)",
+    "ap-northeast-3 (Osaka)",
+    "ap-northeast-2 (Seoul)",
+    "ap-southeast-1 (Singapore)",
+    "ap-southeast-2 (Sydney)",
+    "ap-northeast-1 (Tokyo)",
+    "ca-central-1 (Central)",
+    "eu-central-1 (Frankfurt)",
+    "eu-west-1 (Ireland)",
+    "eu-west-2 (London)",
+    "eu-west-3 (Paris)",
+    "eu-north-1 (Stockholm)",
+    "sa-east-1 (São Paulo)",
+]
 
 class ResourceLoaderThread(QThread):
     resources_loaded = Signal(list)
